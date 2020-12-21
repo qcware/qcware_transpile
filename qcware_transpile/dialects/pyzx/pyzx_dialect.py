e88e81be8db27b753f4bd6816b4cde0ccff21f98
Vic Putz <vbputz@gmail.com>
2020-12-21 15:57:19 -0600

    started adding pyzx

--- qcware_transpile/dialects/pyzx/pyzx_dialect.py
+++ qcware_transpile/dialects/pyzx/pyzx_dialect.py
@@ -5,7 +5,7 @@ from qcware_transpile.instructions import Instruction
 from qcware_transpile.helpers import map_seq_to_seq_unique
 from pyrsistent import pset
 from pyrsistent.typing import PSet, PMap
-from typing import Tuple, Any, Set
+from typing import Tuple, Any, Set, Sequence
 from inspect import isclass, signature
 import numpy as np  # type: ignore
 
@@ -16,18 +16,17 @@ def pyzx_gatethings() -> PSet[Any]:
     """
     The set of all things in qiskit which represent a gate
     """
-    possible_things = pyzx.circuit.gates.__dict__.values()
-    return pset({
-        x
-        for x in possible_things
-        if isclass(x) and issubclass(x, pyzx.circuit.Gate)
-    })
+    possible_things = [
+        x for x in pyzx.circuit.gates.__dict__.values() if isclass(x)
+        and issubclass(x, pyzx.circuit.Gate) and x != pyzx.circuit.Gate
+    ]
+    return possible_things
 
 
 def parameter_names_from_gatething(thing: pyzx.circuit.Gate) -> PSet[str]:
     sig = signature(thing.__init__)
     result = set(sig.parameters.keys()).difference(
-        {'self'})
+        {'self', 'target', 'control', 'ctrl1', 'ctrl2', 'label'})
     return pset(result)
 
 
@@ -36,7 +35,7 @@ def number_of_qubits_from_gatething(thing: pyzx.circuit.Gate) -> int:
     # the gate can operate on, and pretty much the only path forward
     # seems to be to instantiate the gate and see.  That means coming
     # up with fake parameter arguments.
-    param_names = parameter_names_from_gatething(thing)
+    param_names = list(signature(thing.__init__).parameters.keys())
     # in pyzx rather than having a vector of target bits, you have bit names,
     # such as "ctrl1", "ctrl2", "target"
     result = 0
@@ -62,7 +61,7 @@ def gatedef_from_gatething(thing) -> GateDef:
 # some gates are problematic -- in particular Qiskit's "gates" which
 # really just generate other gates, for example those which take a
 # number of bits as an argument.  for now we are disabling those
-Problematic_gatenames = pset({"MSGate"})
+Problematic_gatenames = pset({})
 
 
 def attempt_gatedefs() -> Tuple[PSet[GateDef], PSet[str]]:
@@ -70,7 +69,7 @@ def attempt_gatedefs() -> Tuple[PSet[GateDef], PSet[str]]:
     Iterate through all the gate definitions and build a set of successful
     gate definitions and a list of things which could not be converted
     """
-    all_things = qiskit_gatethings()
+    all_things = pyzx_gatethings()
     success = set()
     failure = set()
     for thing in all_things:
@@ -96,54 +95,93 @@ def gate_names() -> PSet[str]:
 
 def dialect() -> Dialect:
     """
-    The qiskit dialect
+    The pyzx dialect
     """
-    return Dialect(name=__dialect_name__, gate_defs=gate_defs())  # type: ignore
+    return Dialect(name=__dialect_name__,
+                   gate_defs=gate_defs())  # type: ignore
 
 
 def parameter_bindings_from_gate(gate: qiskit.circuit.Gate) -> PMap[str, Any]:
+    # this is a little trickier than others.  Rather than storing parameters
+    # in a dictionary, pyzx stores them as individual Gate members, ie
+    # self.target, self.phi, etc.
+    not_parameters = {'label'}
     values = gate.params
     names = [k for k, v in signature(gate.__init__).parameters.items()
              ][0:len(values)]
     return map_seq_to_seq_unique(names, values)
 
 
-def native_to_circuit(qc: qiskit.QuantumCircuit) -> Circuit:
+def qubit_bindings(g: pyzx.circuit.Gate):
+    """
+    Gets an ordered set of qubit bindings for the given gate.
+    Pyzx, rather sensibly, does gates with names bound to qubits,
+    whereas we usually store an ordered list of qubits.  For pyzx,
+    then,
+    1 qubit -> target
+    2 qubit -> control, target
+    3 qubit -> ctrl1, ctrl2, target
+    """
+    if "ctrl1" in dir(g):
+        result = [g.ctrl1, g.ctrl2, g.target]
+    elif "control" in dir(g):
+        result = [g.control, g.target]
+    else:
+        result = [g.target]
+    return result
+
+
+def native_to_circuit(pc: pyzx.circuit.Circuit) -> Circuit:
     """
     Return a transpile-style Circuit object from a qiskit Circuit object
     """
     instructions = []
     d = dialect()
     valid_names = [g.name for g in d.gate_defs]
-    qc = qc.reverse_bits()
-    for (instruction, qubits, cbits) in qc.data:
-        assert (len(cbits) == 0)
-        gate_name = instruction.__class__.__name__
+    for g in pc.gates:
+        gate_name = g.__class__.__name__
         assert (gate_name in valid_names)
-        gate_def = gatedef_from_gatething(instruction.__class__)
-        parameter_bindings = parameter_bindings_from_gate(instruction)
-        qubit_bindings = [qb.index for qb in qubits]
+        gate_def = gatedef_from_gatething(g.__class__)
+        parameter_bindings = parameter_bindings_from_gate(g)
+        bindings = qubit_bindings(g)
         instructions.append(
             Instruction(
                 gate_def=gate_def,
                 parameter_bindings=parameter_bindings,  # type: ignore
-                bit_bindings=qubit_bindings))
-    qubits = list(range(qc.num_qubits))
-    return Circuit(dialect_name=__dialect_name__,
-                   instructions=instructions,   # type: ignore
-                   qubits=qubits)
+                bit_bindings=bindings))
+    qubits = list(range(pc.qubits))
+    return Circuit(
+        dialect_name=__dialect_name__,
+        instructions=instructions,  # type: ignore
+        qubits=qubits)
+
+def pyzx_qubit_bindings(qubits: Sequence[int]):
+    if len(qubits) == 3:
+        result = {'ctrl1': qubits[0],
+                  'ctrl2': qubits[1],
+                  'target': qubits[2] }
+    elif len(qubits) == 2:
+        result = {'control': qubits[0],
+                  'target' : qubits[1] }
+    elif len(qubits) == 1:
+        result = {'target': qubits[1]}
+    else:
+        assert False
+    return result
 
 
-def qiskit_gate_from_instruction(i: Instruction):
+def pyzx_gate_from_instruction(i: Instruction):
     """
     Create a qiskit Gate object from an instruction
     """
-    gclass = getattr(qiskit.circuit.library, i.gate_def.name)
+    gclass = getattr(pyzx.circuit.gates, i.gate_def.name)
+    parms = i.parameter_bindings
+    parms = parms.update(pyzx_qubit_bindings(i.bit_bindings))
     gate = gclass(**i.parameter_bindings)
     return gate
 
 
-def circuit_to_native(c: Circuit) -> qiskit.QuantumCircuit:
+def circuit_to_native(c: Circuit) -> pyzx.Circuit:
     """
     Make a qiskit circuit from a qcware_transpile Circuit
     """
