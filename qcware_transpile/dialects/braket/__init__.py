0f835fd381b39d79ed228888d6d0b82b4b80726c
Vic Putz <vbputz@gmail.com>
2021-03-15 15:07:14 -0500

    added braket requirements and stubs

---
+++ qcware_transpile/dialects/braket/__init__.py
@@ -0,0 +1,5 @@
+"""
+AWS Braket is Amazon's quantum-on-the-cloud service.
+"""
+# from .braket_dialect import (dialect, native_to_ir, ir_to_native,
+#                              native_circuits_are_equivalent, audit)
