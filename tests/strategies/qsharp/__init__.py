cced56e84af078d78ea0311d9ce70ec9d46d1377
Diana Voronin <dianavoronin5k@gmail.com>
2021-04-30 02:22:46 -0700

    generate Q# circuits

--- tests/strategies/qsharp/__init__.py
+++ tests/strategies/qsharp/__init__.py
@@ -5,10 +5,8 @@ from qcware_transpile.dialects.qsharp.qsharp_dialect import dialect
 
 @composite
 def gates(draw, 
-          min_qubits, 
-          max_qubits,
+          num_qubits,
           gate_list=sorted(dialect().gate_defs)):
-    num_qubits = draw(integers(min_value=min_qubits, max_value=max_qubits))
     gate_def = draw(sampled_from(gate_list).filter(lambda x: len(x.qubit_ids) <= num_qubits))
     qubits = draw(
             lists(integers(min_value=0, max_value=num_qubits-1), 
@@ -25,3 +23,33 @@ def gates(draw,
         param_str = ", ".join("%s" % (a) for a in angles)
         result = gate_def.name + "(" + param_str + ", " + qubit_str + ")"  
     return result 
+
+@composite
+def circuits(draw, 
+             min_qubits, 
+             max_qubits, 
+             min_length, 
+             max_length):
+    length = draw(integers(min_value=min_length, max_value=max_length))
+    length = draw(integers(min_value=min_length, max_value=max_length))
+    num_qubits = draw(integers(min_value=min_qubits, max_value=max_qubits))
+    circuit_gates = draw(lists(gates(num_qubits), min_size=length, max_size=length))
+    result = Template("""
+    namespace NamespaceQFT {
+        open Microsoft.Quantum.Intrinsic;
+        open Microsoft.Quantum.Diagnostics;
+        open Microsoft.Quantum.Math;
+        open Microsoft.Quantum.Arrays;
+
+        operation TestCircuit(): Unit {
+
+            use qs = Qubit[{{num_qubits}}];
+
+            {% for gate in circuit_gates %} 
+            {{gate}} 
+            {% endfor %}
+
+            ResetAll(qs);
+        }
+    """)
+    return result.render(num_qubits=num_qubits, circuit_gates=circuit_gates)
